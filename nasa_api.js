98813af0efa93b2cf7fa0d1fd81c45856acae2f5
niharika829 <49246107+niharika829@users.noreply.github.com>
2019-07-26 21:29:23 +0530

    added function to make a AJAX call in order to fetch background image

--- nasa_api.js
+++ nasa_api.js
@@ -2,3 +2,30 @@
 function reload(){
 	document.location.reload(true);
 	}
+/*function to add background image ,it will take few seconds*/	
+var xhr= new XMLHttpRequest();
+function dog(){
+xhr.open('GET','https://api.nasa.gov/planetary/apod?api_key=FQNny3zHhnUQVZNvhyGhDAikbHC8Zy7wb7NWq8Hx',true);
+
+
+xhr.send();
+xhr.onreadystatechange = function(){
+	
+if(this.readyState == 4 && this.status==200){
+	
+
+									var result= JSON.parse(xhr.responseText);
+
+									console.dir(result);
+
+ 
+									document.getElementById("bd").background = result.url;
+									document.getElementById("notice").style.display = "none";
+									/*sometimes instead of the url, for pic of the day(.jpg format) ,this api provied the url of a video ,
+									which is not supported as a background image so if the background image is not opening please do check
+									from the console if the url provied by this api on that particular time is of .jpg fromat or a video*/ 
+										}
+
+
+};
+}
